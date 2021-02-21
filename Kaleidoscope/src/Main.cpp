68fdd3257fec6133d98bd84cd8c3df1e901da7ef
Arthur Wong <arthurawong55@gmail.com>
2021-02-21 16:31:16 -0700

    fix critical messages and test drawing

--- Kaleidoscope/src/Main.cpp
+++ Kaleidoscope/src/Main.cpp
@@ -1,25 +1,187 @@
 #ifdef KS_PLATFORM_WINDOWS
-
 #include "Log.h"
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <windows.h>
 
+void framebuffer_size_callback(GLFWwindow*, int width, int height);
+void processInput(GLFWwindow* window);
+
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main() {\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+
+const char* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main() {\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
+const unsigned int drawTriangle = 0;
+
 int main(int argc, char** argv) {
 
 	Kaleidoscope::Log::Init();
 	KS_CORE_WARN("Initialized Log!");
-	int a = 10;
-	KS_TRACE("Kaleidoscope Engine = {0}", a);
 
-	glfwInit();
-	glfwWindowHint(GLFW_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_VERSION_MINOR, GLF	);
+	if (!glfwInit()) {
+		KS_CORE_CRITICAL("Failed to initialize GLFW");
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	while (true) {}
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		KS_CORE_CRITICAL("Failed to create GLFW window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		KS_CORE_CRITICAL("Failed to initialize GLAD");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// Vertex shader 
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Verify if vertex shader compile works
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		KS_CORE_CRITICAL("Failed to compile shader vertex");
+		KS_CORE_CRITICAL("{}",infoLog);
+	}
+	// Fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Verify if fragment shader compile works
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		KS_CORE_CRITICAL("Failed to compile fragment shader");
+		KS_CORE_CRITICAL("{}", infoLog);
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Verify if linking shaders works
+	glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		KS_CORE_CRITICAL("Failed to link shaders");
+		KS_CORE_CRITICAL("{}", infoLog);
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Set up vertex data, buffers and configure vertex attributes
+	// If drawTriangle is false, draws rectangle 
+	unsigned int VBO, VAO, EBO;
+	if (drawTriangle) {
+		float verticies[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+		};
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		// First bind VAO, then bind and set vertex buffers, and then configure vertex attributes
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+	}
+	else {
+		float verticies[] = {
+		  0.5f,  0.5f, 0.0f,
+		  0.5f, -0.5f, 0.0f,
+		 -0.5f, -0.5f, 0.0f,
+		 -0.5f,  0.5f, 0.0f
+		};
+		unsigned int indices[] = {
+			0, 1, 3,	//first triangle
+			1, 2, 3,	//second triangle
+		};
+
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+	} 
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glClearColor(0.6f, 0.3f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		if (drawTriangle) {
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+		else {
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		}
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean up resources that no longer have a use
+
+	glDeleteVertexArrays(1, &VAO);
+	if (drawTriangle) {
+		glDeleteBuffers(1, &VBO);
+	}
+	else {
+		glDeleteBuffers(1, &EBO);
+	}
+	glDeleteProgram(shaderProgram);
+
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow*, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
 
 #endif
