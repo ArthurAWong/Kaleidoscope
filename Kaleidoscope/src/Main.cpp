0fb82848e5679988c06a3746dd900440a9c60f3f
Arthur Wong <arthurawong55@gmail.com>
2021-03-01 16:10:01 -0700

    Add shader class and shader file reading

--- Kaleidoscope/src/Main.cpp
+++ Kaleidoscope/src/Main.cpp
@@ -1,31 +1,36 @@
 #ifdef KS_PLATFORM_WINDOWS
+#include <windows.h>
 #include "Log.h"
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-#include <windows.h>
+#include "Shader.h"
 
 void framebuffer_size_callback(GLFWwindow*, int width, int height);
 void processInput(GLFWwindow* window);
-
+/*
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
 	"void main() {\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	gl_Position = vec4(aPos, 1.0);\n"
+	"	ourColor = aColor;\n"
 	"}\0";
 
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
 	"void main() {\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"	FragColor = vec4(ourColor, 1.0);\n"
 	"}\0";
-
-const unsigned int drawTriangle = 0;
+*/
+const unsigned int drawTriangle = 1;
 
 int main(int argc, char** argv) {
 
 	Kaleidoscope::Log::Init();
-	KS_CORE_WARN("Initialized Log!");
+	KS_CORE_INFO("Initialized log");
 
 	if (!glfwInit()) {
 		KS_CORE_CRITICAL("Failed to initialize GLFW");
@@ -50,6 +55,8 @@ int main(int argc, char** argv) {
 	}
 
 	glViewport(0, 0, 800, 600);
+
+	/*
 
 	// Vertex shader 
 	unsigned int vertexShader;
@@ -93,14 +100,19 @@ int main(int argc, char** argv) {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	*/
+
+	Shader ourShader("F:/Dev/Kaleidoscope/Kaleidoscope/src/Shaders/testVert.vert", "F:/Dev/Kaleidoscope/Kaleidoscope/src/Shaders/testFrag.frag");
+
 	// Set up vertex data, buffers and configure vertex attributes
 	// If drawTriangle is false, draws rectangle 
 	unsigned int VBO, VAO, EBO;
 	if (drawTriangle) {
 		float verticies[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		// Position			// Color
+		-0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	// Bottom left
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,	// Bottom right
+		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f	// Top
 		};
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
@@ -109,9 +121,11 @@ int main(int argc, char** argv) {
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
 	}
 	else {
 		float verticies[] = {
@@ -140,13 +154,22 @@ int main(int argc, char** argv) {
 		glEnableVertexAttribArray(0);
 	} 
 
+	float counter = 0;
+
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.6f, 0.3f, 0.4f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		//float redValue = sin(counter/100) / 2.0f + 0.5f;
+		//counter++;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
+		//glUseProgram(shaderProgram);
+		ourShader.use();
+		//glUniform4f(vertexColorLocation, 0.5f, redValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
 		if (drawTriangle) {
 			glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -168,9 +191,10 @@ int main(int argc, char** argv) {
 	else {
 		glDeleteBuffers(1, &EBO);
 	}
-	glDeleteProgram(shaderProgram);
+	ourShader.~Shader();
 
 	glfwTerminate();
+	KS_CORE_INFO("Closed window");
 	return 0;
 }
 
